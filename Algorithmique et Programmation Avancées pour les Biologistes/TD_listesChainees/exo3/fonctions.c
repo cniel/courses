877ed7f86fb547ea39a857d4d2e2ec9fb9b02c61
nielclement <niel.clement@gmail.com>
2013-10-02 13:23:24 +0200

    Tp C.Sinoquet compliqué

--- Algorithmique et Programmation Avancées pour les Biologistes/TD_listesChainees/exo3/fonctions.c
+++ Algorithmique et Programmation Avancées pour les Biologistes/TD_listesChainees/exo3/fonctions.c
@@ -5,33 +5,128 @@
 #include "fonctions.h"
 
 
-void initMat(TMatr* adr_mat, int pnbMaxRows, int pnbMaxCols, double pvalDef){
+void initMat(TMatr* adr_mat, int nbMaxRows, int nbMaxCols, double valDef){
 	//adr_mat = (TMatr*)malloc(sizeof(TMatr));  NE PAS FAIRE ÇA !!!!! : la mémoire est déjà allouée dans le main quand on fait la déclaration.
 	
-	adr_mat->nbMaxRows = pnbMaxRows;
-	adr_mat->nbMaxCols = pnbMaxCols;
-	adr_mat->valDef = pvalDef;
+	adr_mat->nbMaxRows = nbMaxRows;
+	adr_mat->nbMaxCols = nbMaxCols;
+	adr_mat->valDef = valDef;
 	adr_mat->headRows = NULL;
 	
 	printf("Dans la fonction:\nmatrice initialisée :\n%d, %d, %f, %p.\n",adr_mat->nbMaxRows, adr_mat->nbMaxCols, adr_mat->valDef, adr_mat->headRows);
 }
 
-void recherche(TMatr mat, int i, int j, TPRow* adr_pRow, TPRow* adr_pRowPrec, TPCol* adr_pCol, TPCol* adr_pColPrec){
-	*adr_pRow = mat.headRows;
-	*adr_pRowPrec = NULL;
-	*adr_pCol = NULL;
-	*adr_pColPrec = NULL;
-		
-	while(*adr_pRow != NULL && (*adr_pRow)->numRow < i){
-		*adr_pRowPrec = *adr_pRow;
-		*adr_pRow = (*adr_pRow)->next;
+void recherche_col(TPCol headCols, TPCol* adr_Col, TPCol* adr_ColPrec){
+	//postcondition:
+	TPCol pc;
+	TPCol pcp;
+	
+	pcp=NULL; pc=headCols;
+	while(pc != NULL && pc->nulCol < j){
+		pcp = pc; 
+		pc = pc->next;
+	}
+	if(pc != NULL && pc->numCol > j)
+		pc = NULL;
+	*adr_Col=pc;
+	*adr_ColPrec=pcp;	
+}
+
+void recherche(TMatr mat, int i, int j, TPRow* adr_Row, TPRow* adr_RowPrec, TPCol* adr_Col, TPCol* adr_ColPrec){
+	*adr_Row = NULL;
+	*adr_RowPrec = NULL;
+	*adr_Col = NULL;
+	*adr_ColPrec = NULL;
+
+	TPRow pl = mat.headRows;
+	TPRow plp = NULL;
+	TPCol pc = NULL;
+	TPCol pcp = NULL;
+
+
+	while(pl != NULL && pl->numRow < i){
+		plp = pl;
+		pl = pl->next;
 	}
 
-	if(*adr_pRowPrec != NULL){
-		*adr_pCol = (*adr_pRow)->next;
-		while(*adr_pCol != NULL && (*adr_pCol)->numCol < j){
-			*adr_pColPrec = *adr_pCol;
-			*adr_pRowPrec = *adr_pRow;
-		}
+	if(pl != NULL){
+		if(pl->numRow == i) 
+			recherche_col(pl->headCols, adr_Col, adr_ColPrec);
+		else
+			pl=NULL;
+	}
+	
+	*adr_Row = pl;
+	*adr_RowPrec = plp;
+}
+
+void set(TMatr* adr_mat, int i, int j, double val){
+	TPRow ptr_lig_i;
+	TPRow ptr_lig_i_prec;
+	TPCol ptr_col_j;
+	TPCol ptr_col_j_prec;
+	
+	recherche(*adr_mat, i, j, &ptr_lig_i, &ptr_lig_i_prec, &ptr_col_j, &ptr_col_j_prec);
+	
+	if(ptr_lig_i == NULL){
+		if(val != adr_mat->valDef) //1.2
+			creation_case_affectation1(adr_mat, ptr_lig_i_prec, i, j, val);
+		return; //on sort de la procédure.
+	}
+
+	if(ptr_col_j == NULL){
+		if(val != adr_mat->valDef) //1.2
+			creation_case_affectation2(&ptr_lig_i, ptr_col_j_prec, j, val);
+	}else{
+		if(val == adr_mat->valDef) //2.1
+			detruire_case(adr_mat, &ptr_lig_i_prec, &ptr_lig_i, &ptr_col_j_prec, &ptr_col_j);
+		else //2.2
+			modifier_valeur(&ptr_col_j, val);
+	}
+}
+
+void creation_case_affectation1(TMatr* adr_mat, TPRow* adr_p_prec, int i, int j, double val){
+/* préconditions:
+	*adr_p_prec est significatif (NULL ou non)
+	La ligne n'existe pas
+*/
+
+	TPRow p = allouerMemoire();
+	TPCol q = allouerMemoire();
+	
+	p->numRow = i;
+	if(*adr_p_prec == NULL){
+		p->next = adr_mat->headRows;
+		adr_mat->headRows = p;
+	}else{
+		p->next = *adr_p_prec->next;
+		*adr_p_prec->next = p;
+	}
+	//création de la colonne j
+	q->val = val;
+	q->numCol = j;
+	q->next = NULL;
+	p->headCols = q;
+}
+
+void creation_case_affectation2(TPRow* adr_ptr_lig_i, TPCol* adr_p_j_prec, int j, double val){
+/* préconditions:
+	*adr_ptr_lig_i != NULL (et est significatif)
+	*adr_p_j_prec est significatif (NULL ou non)
+
+   Postcondition:
+   	la colonne j a été créé.
+*/
+
+	TPCol q = allouerMemoire();
+	q->val=val;
+	q->numCol=j;
+	
+	if(*adr_p_j_prec == NULL){
+		q->next = adr_ptr_lig_i->headCols;
+		*adr_ptr_lig_i->headCols = q;
+	}else{
+		q->next = *adr_p_j_prec->next;
+		*adr_p_j_prec->next = q;
 	}
 }
--- Algorithmique et Programmation Avancées pour les Biologistes/TD_listesChainees/exo3/fonctions.h
+++ Algorithmique et Programmation Avancées pour les Biologistes/TD_listesChainees/exo3/fonctions.h
@@ -1,27 +1,12 @@
+void initMat(TMatr* adr_mat, int nbMaxRows, int nbMaxCols, double valDef);
+
+void creation_case_affectation1(TMatr* adr_mat, TPRow* adr_p_prec, int i, int j, double val);
+void creation_case_affectation2(TPRow* adr_ptr_lig_i, TPCol* adr_p_j_prec, int j, double val){
+
+void recherche_col(TPCol headCols, TPCol* adr_Col, TPCol* adr_ColPrec);
 
 
-void initMat(TMatr* adr_mat, int pnbMaxRows, int pnbMaxCols, double pvalDef);
-
-void set(TMatr* adr_mat, int i, int j, double val); //mat[i][j] = val;
-/*
-ANALYSE de la fonction set:
-
-	SI on remplace une valeur par défaut par une valeur par défaut, ALORS il n'y a rien à faire.
-	
-	SI on remplace une valeur particulière par la valeur par défaut, ALORS il faut faire de la destruction :
-	-supprimer la cellule
-	-et peut-être aussi sauter sa ligne si c'était sa dernière cellule.
-	
-	SI on remplace la valeur par défaut par une particulière, ALORS il faut faire de la création :
-	-peut-être créer une cellule ligne.
-	-créer une cellule colonne.
-
-pointeur courant et pointeur précédent : 2 pour les lignes, 2 pour les colonnes.
-
-c'est pas de la bonne prog d'avoir une fonction qui fasse un return ET qui ait des paramètres par adresse.
-*/
-
-void recherche(TMatr adr_mat, int i, int j, TPRow* adr_pRow, TPRow* adr_pRowprec, TPCol* adr_pCol, TPCol* adr_pColPrec);
+void recherche(TMatr mat, int i, int j, TPRow* adr_Row, TPRow* adr_RowPrec, TPCol* adr_Col, TPCol* adr_ColPrec);
 /*postCondition:
 si *adr_pRow = NULL   :  la ligne i n'est pas présente dans la structure.
 si ________ != NULL   :  _________ est présente.
@@ -32,3 +17,26 @@ si *adr_pCol != NULL  :  la colonne j est présente.
 si *adr_pRowPrec != NULL : valeur particulière.
 si *adr_pColPrec != NULL : valeur particulière.
 */
+
+
+void set(TMatr* adr_mat, int i, int j, double val); //mat[i][j] = val;
+
+/*
+ANALYSE de la fonction set:
+
+	SI on remplace une valeur par défaut par une valeur par défaut, ALORS il n'y a rien à faire. 		//1.1
+	SI on remplace la valeur par défaut par une particulière, ALORS il faut faire de la création :		//1.2
+	-peut-être créer une cellule ligne (la ligne i si elle n'existe pas).
+	-créer une cellule colonne (la colonne j).
+	
+	SI on remplace une valeur particulière par la valeur par défaut, ALORS il faut faire de la destruction :	//2.1
+	-supprimer la cellule
+	-et peut-être aussi sauter sa ligne si c'était sa dernière cellule.
+	
+	SI on remplace une valeur particulière par une valeur particulière, ALORS il faut faire de la mise à jour. 	//2.2
+	
+	
+C'est pas de la bonne prog d'avoir une fonction qui fasse un return ET qui ait des paramètres par adresse.
+*/
+
+
